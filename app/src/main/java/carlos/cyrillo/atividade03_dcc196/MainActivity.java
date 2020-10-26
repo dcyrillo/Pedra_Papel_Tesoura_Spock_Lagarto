488dfe6bb8d51babf14ac56b9ae1ee3f55f8b914
Carlos Eduardo Magalhães Cyrillo <carlos.cyrillo@haizen.tech>
2020-10-26 17:51:47 -0300

    Iniciando atividade 3
    -Pedra, Papel e tesoura

---
+++ app/src/main/java/carlos/cyrillo/atividade03_dcc196/MainActivity.java
@@ -0,0 +1,28 @@
+package carlos.cyrillo.atividade03_dcc196;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+}
